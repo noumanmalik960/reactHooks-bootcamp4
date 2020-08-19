e8f6ede103f3c542a70c78abec6043dbe7b8f091
Nouman Javed <noumanmalik960@gmail.com>
2020-08-19 15:12:57 +0500

    updated all tasks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Room() {
+
+  let [isLit, setLit] = useState(true);
+  let [age, setAge] = useState(19);
+  let [temp, setTemp] = useState(22);
+  const brightness = isLit ? "lit": "dark"
+
+  return (
+    <div className={`room ${brightness}`}>
+      <br/>
+      <h2>The room is {isLit ? "LIT" : "DARK"}</h2>
+      <br />
+      <button onClick={() => setLit(true)}>On</button>
+      <button onClick={() => setLit(!isLit)}>flip</button>
+      <button onClick={() => setLit(false)}>Off</button>
+      <hr />
+
+      <h2>The age is: {age}</h2>
+      <button onClick={() => setAge(++age)}>incrAge</button>
+      <button onClick={() => setAge(19)}>resetAge</button>
+      <hr />
+
+      <h2>Temperature is: {temp}</h2>
+      <button onClick={() => setTemp(++temp)}>+</button>
+      <button onClick={() => setTemp(22)}>reset</button>
+      <button onClick={() => setTemp(--temp)}>-</button>
+      
+      <hr />
+
+    </div>
+  );
+}
+
+
+ReactDOM.render(<Room />, document.getElementById('root'));
